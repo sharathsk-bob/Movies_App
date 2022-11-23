e9b669ae025ff3e86f6c64a8131aa8560fa1487f
Sashi Kumar <sharath.sashi-kumar@capgemini.com>
2022-11-23 10:23:17 +0530

    app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="search_nav">
+        <div>
+          <h1>Movies</h1>
+        </div>
+        <div>
+          <form>
+            <button>Search</button>
+          </form>
+        </div>
+      </div>
+      <div className="movies">
+        
+      </div>
       
     </div>
   );
