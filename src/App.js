23b922bb9897c69b6bcbda4c462eafce4a2501ef
Sashi Kumar <sharath.sashi-kumar@capgemini.com>
2022-11-24 12:00:34 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
-//hello
+import {useEffect, useState} from 'react';
+import MovieCard from './MovieCard';
 function App() {
+  const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=48a523509844cfa4da8192188b6adb4a"
+  const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=48a523509844cfa4da8192188b6adb4a&query="
+  const [movies,setMovies]=useState([]);
+  const [term,setTerm]=useState('');
+  const handleSearch=(e)=>{
+      e.preventDefault();
+      fetch(API_SEARCH+term)
+      .then(res=>res.json())
+      .then(data=>setMovies(data.results));
+  }
+  useEffect(()=>{
+    fetch(API_URL).then(res=>res.json()).then(data=>setMovies(data.results))
+  },[])
+  console.log(movies)
   return (
     <div className="App">
       <div className="search_nav">
-        <div>
+        <div className='title'>
           <h1>Movies</h1>
         </div>
-        <div>
-          <form>
+        <div className='search_box'>
+          <form onSubmit={handleSearch}>
+            <input on onChange={(e)=>setTerm(e.target.value)}/>
             <button>Search</button>
           </form>
         </div>
       </div>
       <div className="movies">
-        
+        {movies.map((movie)=>(
+            <MovieCard {...movie}/>
+        ))}
       </div>
       
     </div>
---
+++ src/MovieCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './MovieCard.css'
+
+function MovieCard(props) {
+const API_IMAGE="https://image.tmdb.org/t/p/w500"
+  return (
+    <div className='card'>
+        <div className='poster'>
+            <img src={props.poster_path? API_IMAGE+props.poster_path:"https://images.unsplash.com/photo-1597575732103-9f6d068cfa9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV0ZmxpeHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"}/>
+        </div>
+        <div className='info'>
+            <p className='title'>{props.title}</p>
+            <p className='vote'>{props.vote_average}</p>
+        </div>
+        <div className='overview'>
+          <h2 className='title_overview'>OverView</h2>
+          <h3 className='overview_info'>{props.overview}</h3>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard
