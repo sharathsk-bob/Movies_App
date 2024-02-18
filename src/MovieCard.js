258e434f3fd44df5930cdb07cdb849e1c68f7c7e
sharathsk-bob <66692542+sharathsk-bob@users.noreply.github.com>
2024-02-18 10:20:44 +0530

    Update MovieCard.js

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -6,7 +6,7 @@ const API_IMAGE="https://image.tmdb.org/t/p/w500"
   return (
     <div className='card'>
         <div className='poster'>
-            <img src={props.poster_path? API_IMAGE+props.poster_path:"https://images.unsplash.com/photo-1597575732103-9f6d068cfa9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV0ZmxpeHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"}/>
+            <img src={props.poster_path? API_IMAGE+props.poster_path:"https://images.unsplash.com/photo-1597575732103-9f6d068cfa9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV0ZmxpeHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"} alt="cardImage"/>
         </div>
         <div className='info'>
             <p className='title'>{props.title}</p>
